1f2f6989fa527224b49c94c3ff0b98ec84c9fd17
Francsy <francsyludens@gmail.com>
2024-01-03 20:47:31 +0000

    add/prevous and next question navegation logic with global state

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,15 +1,20 @@
-import { Card, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
+import { Card, IconButton, List, ListItem, ListItemButton, ListItemText, Stack, Typography } from '@mui/material';
 import { useQuestionsStore } from "./store/questions";
 import SyntaxHighLighter from "react-syntax-highlighter";
 import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { type Question as QuestionType } from "./types";
+import { ArrowBackIosNew, ArrowForwardIos } from '@mui/icons-material';
 
 // We create this function outside to not create it every time we render the Question component:
 const getBackgroundColor = (info: QuestionType, index: number) => {
     const { userSelectedAnswer, correctAnswer } = info;
+    // if user doesn't select anything yet:
     if (userSelectedAnswer == null) return 'transparent';
+    // if this is not the selected option nor the correct one and user already made its choice:
     if (index !== correctAnswer && index !== userSelectedAnswer) return 'transparent';
+    // If this is the user selection and it's correct:
     if (index === correctAnswer) return 'green';
+    // If this is the user selection but it's not correct:
     if (index === userSelectedAnswer) return 'red';
     return 'transparent';
 };
@@ -56,11 +61,24 @@ const Question = ({ info }: { info: QuestionType }) => {
 export const Game = () => {
     const questions = useQuestionsStore(state => state.questions);
     const currentQuestion = useQuestionsStore(state => state.currentQuestion);
+    const goNextQuestion = useQuestionsStore(state => state.goNextQuestion);
+    const goPreviousQuestion = useQuestionsStore(state => state.goPreviousQuestion);
+
     const questionInfo = questions[currentQuestion];
-    console.log(questions);
 
     return (
         <>
+            <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+                <IconButton onClick={goPreviousQuestion} disabled={currentQuestion === 0}>
+                    <ArrowBackIosNew />
+                </IconButton>
+                {currentQuestion + 1} / {questions.length}
+                <IconButton onClick={goNextQuestion} disabled={currentQuestion >= questions.length - 1}>
+                    <ArrowForwardIos />
+                </IconButton>
+
+
+            </Stack>
             <Question info={questionInfo} />
         </>
     );
